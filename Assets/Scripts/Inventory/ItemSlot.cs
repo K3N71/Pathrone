b1240d92f3186e8ac68ba5becd6fce4d8e739d8e
Frankie Alfano <falfanoiii@gmail.com>
2023-03-31 13:53:34 -0400

    Minor changes

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -4,44 +4,65 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour {
-	[Header("Components")]
-	[SerializeField] private Image itemImage = null;
-	[SerializeField] private CursorItemSlot cursorItemSlot = null;
-	[SerializeField] private Inventory inventory = null;
-	[Header("Properties")]
-	[SerializeField] private ItemScriptableObject item = null;
+    [Header("Components")]
+    [SerializeField] private Image itemImage = null;
+    [SerializeField] private CursorItemSlot cursorItemSlot = null;
+    [SerializeField] private Inventory inventory = null;
+    [Header("Properties")]
+    [SerializeField] private ItemScriptableObject item = null;
+    [SerializeField] private int x = 0;
+    [SerializeField] private int y = 0;
+    [SerializeField] private List<ItemSlot> linkedItemSlots = new List<ItemSlot>( );
 
-	#region Properties
-	public ItemScriptableObject Item {
-		get => item;
-		set {
-			if (value != null) {
-				/*for (int x = 0; x < item.Size.x; x++) {
-					for (int y = 0; y < item.Size.y; y++) {
-						if (inventory[x, y].Item != null) {
-							return;
-						}
-					}
-				}*/
+    #region Properties
+    public int X => x;
+    public int Y => y;
+    public ItemScriptableObject Item {
+        get => item;
+        set {
+            item = value;
+            itemImage.enabled = (value != null);
 
-				itemImage.sprite = value.Sprite;
-			}
+            if (value != null) {
+                itemImage.sprite = value.Sprite;
+            }
+        }
+    }
+    #endregion
 
-			item = value;
-			itemImage.enabled = (value != null);
-		}
-	}
-	#endregion
+    protected void OnValidate ( ) {
+        Item = Item;
+    }
 
-	protected void OnValidate ( ) {
-		Item = Item;
-	}
+    protected void OnMouseDown ( ) {
+        SwapItem(cursorItemSlot);
+    }
 
-	protected void OnMouseDown ( ) {
-		SwapItem(cursorItemSlot);
-	}
+    private void SwapItem (ItemSlot itemSlot) {
+        (itemSlot.Item, Item) = (Item, itemSlot.Item);
+    }
 
-	private void SwapItem (ItemSlot itemSlot) {
-		(itemSlot.Item, Item) = (Item, itemSlot.Item);
-	}
+    private bool SetItem (ItemScriptableObject item, int x, int y) {
+        // A list of the item slots that will be effected by the item being added
+        List<ItemSlot> itemSlots = new List<ItemSlot>( );
+
+        // Loop through and check to make sure that all item slots the current held item will cover are free
+        for (int i = 0; i < item.Size.x; i++) {
+            for (int j = 0; j < item.Size.y; j++) {
+                // If the item slot is not null, then the slot already has an item in it and cannot be set
+                if (inventory[x + i, y + j].Item != null) {
+                    return false;
+                }
+
+                // If the item slot is free, then add it to the list of item slots
+                itemSlots.Add(inventory[x + i, y + j]);
+            }
+        }
+
+        for (int i = 0; i < item.Size.x * item.Size.y; i++) {
+            itemSlots[i].Item = Item;
+        }
+
+        return true;
+    }
 }
